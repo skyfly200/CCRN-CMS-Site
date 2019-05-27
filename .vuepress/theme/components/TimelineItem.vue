65a012d3c65aa9f75412f61cd525999c23176bfa
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-26 21:38:30 -0600

    improve timeline item layout

--- .vuepress/theme/components/TimelineItem.vue
+++ .vuepress/theme/components/TimelineItem.vue
@@ -2,14 +2,16 @@
   <v-card>
     <v-img v-if="frontmatter.image" :src="$withBase(frontmatter.image)" alt="Timeline item image" height="200px" class="white--text" />
     <v-card-title>
-      <h1> {{ frontmatter.title }} </h1>
+        <h1>{{ frontmatter.title }}</h1>
+        <v-spacer></v-spacer>
+        <h2 v-if="!feature">{{ frontmatter.date }}</h2>
     </v-card-title>
     <v-card-text>
-      <slot></slot>
+        <slot></slot>
     </v-card-text>
     <v-divider></v-divider>
     <v-card-actions v-if="feature">
-      <v-btn :to="path" depressed color="primary">Read More</v-btn>
+        <v-btn :to="path" depressed color="primary">Read More</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -19,4 +21,8 @@ export default {
 };
 </script>
 <style>
+.v-cart__title {
+    display: flex;
+    flex-direction: column;
+}
 </style>
