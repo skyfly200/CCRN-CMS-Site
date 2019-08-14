1168e8e26c3004fb88f80b8126696fc80bf5c3d0
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-08-14 13:01:59 -0600

    fix broke styles

--- .vuepress/theme/components/Nav.vue
+++ .vuepress/theme/components/Nav.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="nav-layout">
-    <v-navigation-drawer app v-model="drawer">
+    <v-navigation-drawer app dark v-model="drawer">
       <v-list>
-        <v-list-tile
-          v-for="page in menuPages"
-          :key="page.text"
-          :to="page.link">
-            <v-list-tile-action v-if="page.icon">
-              <v-icon> {{ page.icon }} </v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title> {{ page.text }} </v-list-tile-title>
-            </v-list-tile-content>
+        <v-list-tile v-for="page in menuPages" :key="page.text" :to="page.link">
+          <v-list-tile-action v-if="page.icon">
+            <v-icon>{{ page.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ page.text }}</v-list-tile-title>
+          </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -22,37 +19,37 @@
       <v-toolbar-title>{{ $site.title }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat
-          v-for="page in menuPages"
-          :key="page.text"
-          :to="page.link">
-            {{ page.text }}
-        </v-btn>
+        <v-btn flat v-for="page in menuPages" :key="page.text" :to="page.link">{{ page.text }}</v-btn>
       </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
 <script>
-  export default {
-      computed: {
-          menuPages() {
-            return this.$site.themeConfig.nav || [];
-          }
-      },
-      data: function() {
-        return {
-          drawer: false
-        };
-      },
-      mounted () {
-        this.$router.afterEach(() => {
-          this.drawer = false
-        })
-      },
-  };
+export default {
+  computed: {
+    menuPages() {
+      return this.$site.themeConfig.nav || [];
+    }
+  },
+  data: function() {
+    return {
+      drawer: false
+    };
+  },
+  mounted() {
+    this.$router.afterEach(() => {
+      this.drawer = false;
+    });
+  }
+};
 </script>
 <style>
-  .v-toolbar .v-btn {
-    color: #FFF!important;
-  }
+.v-toolbar .v-btn {
+  color: #fff !important;
+  background-color: transparent !important;
+}
+.v-btn:before {
+  border-radius: 0;
+  background-color: transparent;
+}
 </style>
